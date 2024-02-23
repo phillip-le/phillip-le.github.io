fd00838a757068cb3519bc741b41c095bf542df7
Phillip Le <phillip.huy.le@gmail.com>
2024-02-24 10:37:28 +1100

    Update site to https://phillip-le.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/phillip-le/phillip-le",
+  site: "https://phillip-le.github.io",
   integrations: [
     react(),
     tailwind(),
