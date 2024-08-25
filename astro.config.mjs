d0a9d92d3eda76f5be3ac478b964726006a1244c
Phillip Le <53925279+phillip-le@users.noreply.github.com>
2024-08-25 20:48:10 +1000

    Add package manager and link anchor plugins (#89)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 import starlight from '@astrojs/starlight';
 import starlightLinksValidator from 'starlight-links-validator';
@@ -17,10 +19,23 @@ export default defineConfig({
       customCss: [
         './src/tailwind.css',
         '@fontsource-variable/jetbrains-mono/index.css',
+        './src/styles/headings.css',
       ],
       plugins: [starlightLinksValidator()],
     }),
   ],
+  markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+      [
+        rehypeAutolinkHeadings,
+        {
+          // Wrap the heading text in a link.
+          behavior: 'wrap',
+        },
+      ],
+    ],
+  },
   vite: {
     optimizeDeps: {
       // fixes error when running `nr start` where `node` for `msw/node` cannot be found with `vite:dep-scan`
