ce735144598e59f7792e41458967e4d89a714b0b
Phillip Le <phillip.huy.le@gmail.com>
2024-10-15 20:12:44 +1100

    Update prettier config to use prettier-plugin-astro

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,7 +1,16 @@
+/** @type {import("prettier").Config} */
 export default {
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'all',
   printWidth: 80,
   plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 };
