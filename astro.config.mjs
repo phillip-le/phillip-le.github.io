c3db16a972d20392f83495d2664e3e8a04a5e3e3
Phillip Le <53925279+phillip-le@users.noreply.github.com>
2026-01-31 09:40:43 +1100

    build-deps-tailwind (#263)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import tailwindcss from '@tailwindcss/vite';
 
 import starlight from '@astrojs/starlight';
 import starlightLinksValidator from 'starlight-links-validator';
@@ -12,14 +12,12 @@ export default defineConfig({
   site: 'https://phillip-le.github.io',
   integrations: [
     react(),
-    tailwind({ applyBaseStyles: false }),
     starlight({
       title: 'Developer Musings',
       lastUpdated: true,
       customCss: [
         './src/tailwind.css',
-        '@fontsource-variable/jetbrains-mono/index.css',
-        './src/styles/headings.css',
+        '@fontsource-variable/jetbrains-mono/index.css'
       ],
       plugins: [starlightLinksValidator()],
     }),
@@ -27,26 +25,11 @@ export default defineConfig({
   markdown: {
     rehypePlugins: [
       rehypeHeadingIds,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: 'wrap', // Wrap the heading with a link
-          properties: {
-            className: 'heading-link', // Optional: apply a class for the link
-          },
-          content: [
-            {
-              type: 'element',
-              tagName: 'span', // Create a span element to wrap the #
-              properties: { className: ['hash'] }, // Add a class to the span for styling
-              children: [{ type: 'text', value: '#' }], // The actual # symbol
-            },
-          ],
-        },
-      ],
+      rehypeAutolinkHeadings,
     ],
   },
   vite: {
+    plugins: [tailwindcss()],
     optimizeDeps: {
       // fixes error when running `nr start` where `node` for `msw/node` cannot be found with `vite:dep-scan`
       // https://github.com/vitejs/vite/issues/14151
