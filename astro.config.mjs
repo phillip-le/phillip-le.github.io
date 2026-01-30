a88870b42edf00efddb399f5c0df3a482c8c959b
Phillip Le <phillip.huy.le@gmail.com>
2026-01-31 09:40:15 +1100

    feat: Remove # from headers because link anchors now appear by default

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,8 +17,7 @@ export default defineConfig({
       lastUpdated: true,
       customCss: [
         './src/tailwind.css',
-        '@fontsource-variable/jetbrains-mono/index.css',
-        './src/styles/headings.css',
+        '@fontsource-variable/jetbrains-mono/index.css'
       ],
       plugins: [starlightLinksValidator()],
     }),
@@ -26,23 +25,7 @@ export default defineConfig({
   markdown: {
     rehypePlugins: [
       rehypeHeadingIds,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: 'wrap', // Wrap the heading with a link
-          properties: {
-            className: 'heading-link', // Optional: apply a class for the link
-          },
-          content: [
-            {
-              type: 'element',
-              tagName: 'span', // Create a span element to wrap the #
-              properties: { className: ['hash'] }, // Add a class to the span for styling
-              children: [{ type: 'text', value: '#' }], // The actual # symbol
-            },
-          ],
-        },
-      ],
+      rehypeAutolinkHeadings,
     ],
   },
   vite: {
